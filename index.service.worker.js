6400016bd6f3d90ddd13f23d083c21f933508385
TWS2401 <joshuabetito2401@gmail.com>
2023-12-27 00:01:00 +0000

    Fix file sharing bug
    more of a workaround but whatevs

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1703632983|4047717393";
+const CACHE_VERSION = "1703634971|6035992207";
 const CACHE_PREFIX = "OG!Academytale --sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
